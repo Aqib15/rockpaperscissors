8468a453208114adfd56433b0440a7ab603a06c1
Glitch (rockpaperscissorsaqib) <none>
2021-11-30 23:25:04 +0000

    🐣📰 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+$(".play").click(function(){  
+   let input = $(".input").val();     
+   $(".userChoice").text(input)
+  let computerNumber = Math.ceil(Math.random()*3)
+   $(".computerChoice").text(computerNumber)
+});
 
