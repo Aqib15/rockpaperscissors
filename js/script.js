fc60ab82ac017149a05d1fdb5d9fa3abb2418b4e
Glitch (rockpaperscissorsaqib) <none>
2022-01-11 23:23:14 +0000

    🎚️📁 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -3,32 +3,70 @@ $(".play").click(function() {
   $(".userChoice").text(input);
   
   let computerNumber = Math.ceil(Math.random() * 3);
+  let computerChoice;
   $(".computerChoice").text(computerNumber);
   
   if (computerNumber === 1) {
-    $(".computerChoice").text("Rock");
+    computerChoice = 'rock';
+    $(".computerChoice").text("rock");
      $(".computerImage").attr("src", 
     "https://cdn.glitch.me/d46cdaf2-6d54-4e61-9125-9dd9694a67ac%2Frock.jpg?v=1638918025270");
   }
   else if (computerNumber === 2) {
-    $(".computerChoice").text("Paper");
+     computerChoice = 'paper';
+    $(".computerChoice").text("paper");
     $(".computerImage").attr("src", 
   "https://cdn.glitch.me/d46cdaf2-6d54-4e61-9125-9dd9694a67ac%2Fpaper.png?v=1638917717175");
   }
   
 
 else if (computerNumber === 3) {
-    $(".computerChoice").text("Scissor");
+   computerChoice = 'scissor';
+    $(".computerChoice").text("scissor");
      $(".computerImage").attr("src", 
   "https://cdn.glitch.me/d46cdaf2-6d54-4e61-9125-9dd9694a67ac%2Fscissors.png?v=1638917914812"); 
   }
   
-  if(input === "Rock" && $(".computerChoice").val()=== "Rock"){
-    $(".result").text("tie");
-}
+  determineWinner(input, computerChoice);
   
   
   
   
   
 });
+
+function determineWinner(userChoice, computerChoice){
+    if(userChoice === "rock" && computerChoice === "rock"){
+    $(".result").text("Tie");  
+    return determineWinner;
+    }
+    else if(userChoice === "paper" && computerChoice === "paper"){
+    $(".result").text("Tie");    
+  }
+  else if(userChoice === "scissor" && computerChoice === "scissor"){
+    $(".result").text("Tie");     
+  }
+  else if(userChoice === "rock" && computerChoice === "scissor"){
+    $(".result").text("User Wins!");      
+  }
+  else if(userChoice === "paper" && computerChoice === "rock"){
+    $(".result").text("User Wins!")
+  }
+  else if(userChoice === "scissor"&& computerChoice === "paper"){
+    $(".result").text("User Wins!")
+  }
+  else if(userChoice === "rock" && computerChoice === "paper"){
+    $(".result").text("Computer Wins!");
+  }
+  else if(userChoice === "paper" && computerChoice === "scissor"){
+    $(".result").text("Computer Wins!");
+  }
+  else if(userChoice === "scissor" && computerChoice === "rock"){
+    $(".result").text("Computer Wins!");
+  }
+  else{
+    return $(".result").text("invalid option");
+  }
+  
+  
+}
